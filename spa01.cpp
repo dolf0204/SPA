381ea37c4db7536712cda3841c3ec200f208d74f
dolf0204 <37153771+dolf0204@users.noreply.github.com>
2018-03-07 19:45:17 +0100

    Create spa01.cpp

---
+++ spa01.cpp
@@ -0,0 +1,17 @@
+#include"ComplexNumber.h"
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+	ComplexNumber k1(17, 2);
+	ComplexNumber k2;
+	k2.set_real(9);
+	k2.set_imaginary(-2);
+	ComplexNumber rez(k1, k2);
+	rez.get();
+
+	system("pause");
+	return 0;
+}
