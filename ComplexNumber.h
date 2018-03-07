8c4aed21821427ba7d885a4fbc4fa5674fafc733
dolf0204 <37153771+dolf0204@users.noreply.github.com>
2018-03-07 19:45:57 +0100

    Create ComplexNumber.h

---
+++ ComplexNumber.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class ComplexNumber
+{
+public:
+	ComplexNumber(int a, int b);
+	ComplexNumber();
+	void set_real(int a);
+	void set_imaginary(int b);
+	ComplexNumber(ComplexNumber, ComplexNumber);
+	void get();
+private:
+	int a, b, c, d;
+
+
+
+};
